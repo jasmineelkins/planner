61bf6baa356c32a47b7f2109d7a1f0aeeee5f56c
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-05-04 16:18:05 -0400

    Followed along tutorial - final commit before deploying

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   resources :notes
   resources :tasks
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '*path',
+      to: 'application#fallback_index_html',
+      constraints: ->(request) { !request.xhr? && request.format.html? }
 end
